e3f1bd76ebe2eb16a0ede36445eb9109cea66f19
Unofficialret3 <138513726+Unofficialret3@users.noreply.github.com>
2025-05-20 17:30:54 +0200

    `Refactored HomeScreen.java to include background image and updated layout, and added title to SpaceInvaders.java frame.`

--- SpaceInvaders.java
+++ SpaceInvaders.java
@@ -18,9 +18,10 @@ public class SpaceInvaders {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(sizeX, sizeY);
         frame.setResizable(false);
+        frame.setTitle("TIM DER FURRY SLAYER very HD 69FPS!!! EXTREME OPEN BETA ALPHA V 4.20");
 
         //homescreen
-       // HomeScreenActivate();
+       //HomeScreenActivate();
         
        GamePanelActivate();
     }
